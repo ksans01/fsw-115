66c345385a58205cf2e36fe8d62cd35d412dfcc5
ksans01 <kurt.sanson@bryanuniversity.edu>
2022-01-08 22:03:58 -0600

    api

---
+++ week2/postman.js
@@ -0,0 +1,29 @@
+
+window.onload = () => {
+    randomDogFacts()
+}
+
+const randomDogFacts = () => {
+
+
+document.getElementById("api").innerHTML = 
+
+[
+    {
+        "fact": "A German Shepherd guide dog led her blind companion the entire 2,100 mile Appalachian Trail."
+    },
+    {
+        "fact": "National Geographic's Dr. Brady Barr measured a dog's average bite force at 320 pounds of pressure per square inch."
+    },
+    {
+        "fact": "Dogs’ sense of hearing is more than ten times more acute than a human’s."
+    },
+    {
+        "fact": "Hollywood’s first and arguably best canine superstar was Rin Tin Tin, a five-day-old German Shepherd found wounded in battle in WWI France and adopted by an American soldier, Lee Duncan. He would sign his own contracts with his paw print."
+    },
+    {
+        "fact": "Dalmatians are completely white at birth."
+    }
+]
+
+}
