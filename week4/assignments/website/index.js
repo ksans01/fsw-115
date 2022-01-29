4fd18c8b799e48bcd1e312cc2a3b22bb5e294b89
ksans01 <kurt.sanson@bryanuniversity.edu>
2022-01-29 09:58:06 -0600

    Website Update

--- week4/assignments/website/index.js
+++ week4/assignments/website/index.js
@@ -1,23 +1,27 @@
 
+const getSimpsonsCharacters = () => {
+    axios.get('https://api.sampleapis.com/simpsons/characters')
+    .then(response => {
+        for (let i=0; i<25; i++){
+            const h2 = document.createElement('h2')
+            h2.textContent = response.data[i].name
+            document.body.appendChild(h2)
+        }
+            })
+    .catch(error => console.log(error))
+}
 
-var page1 = axios.get('https://api.sampleapis.com/simpsons/characters')
-.then(response => {
-    for (let i=0; i<25; i++){
-        const h2 = document.createElement('h2')
-        h2.textContent = response.data[i].name
-        document.body.appendChild(h2)
+const getMemes = () => {
+    axios.get('https://api.imgflip.com/get_memes')
+    .then(response => {
+        const memesArray = response.data.data.memes
+        for (let i=0; i<memesArray.length; i++){
+            const h3 = document.createElement('h3')
+            h3.textContent = memesArray[i].name
+            document.body.appendChild(h3)
     }
 })
 .catch(error => console.log(error))
 
-
-var page2 = axios.get('https://api.imgflip.com/get_memes')
-.then(response => {
-    for (let i=0; i<response.data.length; i++){
-        const h3 = document.createElement('h3')
-        h3.textContent = response.data[i].name
-        document.body.appendChild(h3)
-    }
-})
-.catch(error => console.log(error))
+}
 
