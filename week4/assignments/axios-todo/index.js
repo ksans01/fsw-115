5819114bf825d349a86ce0e769d76aa8671e37f8
ksans01 <kurt.sanson@bryanuniversity.edu>
2022-01-29 10:26:33 -0600

    Axios To Do update

--- week4/assignments/axios-todo/index.js
+++ week4/assignments/axios-todo/index.js
@@ -5,6 +5,10 @@ axios.get('http://api.bryanuniversity.edu/kurt/list/')
     for(let i = 0; i < response.data.length; i++){
         const ol = document.createElement('ol')
         ol.textContent = response.data[i].name
+
+        if(response.data[i].isComplete){
+            ol.classList.add('item-complete')
+        }
         document.body.appendChild(ol)
     }
 })
